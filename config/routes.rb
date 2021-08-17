d6b18d6c53c254bb0bc781322d332d880e253f15
aaron staes <aaron.staes@gmail.com>
2021-08-17 14:41:40 +0200

    refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Read all
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
 
-  # Create
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # Create
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # Read one
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Read one
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # Edit
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Edit
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
