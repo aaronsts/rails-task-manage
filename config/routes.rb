db53f9cf8a923fc0f78ace5b360d911e8e71a4b5
aaron staes <aaron.staes@gmail.com>
2021-08-17 12:40:39 +0200

    added paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read all
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Edit
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
